df10ac891a8580133d25227c933965a0677d8094
tofuDriver <007kyg2@naver.com>
2024-10-14 09:07:17 +0900

    2nd calculator ready for sequence test

---
+++ js/dateToHistory.js
@@ -0,0 +1,43 @@
+let history = [];//배열
+let historyIndex =-1;//인덱스
+
+function saveHistory(){
+    const ctext = document.getElementById('addSubject').value;
+    if (ctext && (historyIndex === -1 || history[historyIndex] !== ctext)) {
+        history.push(ctext);
+        //히스토리 배열에 디스플레이된 문자열을 추가하는 방식
+        historyIndex = history.length - 1; // 새로운 입력이므로 인덱스를 업데이트
+        updateHistoryDisplay();
+        document.getElementById('addSubject').value = ''; // 입력 필드 초기화
+        console.log("입력필드가 초기화되었습니다.");
+        
+    }
+    
+}
+
+function updateHistoryDisplay() {
+    const hisd = document.getElementById('historyDisplay');
+    hisd.innerHTML = 'History:<br>' + history.join('<br>');
+    //History Display 새로쓰기
+    //매회차마다 새로씀
+}
+
+function viewHistory(){
+    if (history.length > 0) {
+        const blob = new Blob([history.join('\n')], { type: 'text/plain' });
+        //바이너리데이터 저장을 (데이터 배열, 데이터 처리 타입) 에 맞춰서 할것입니다.
+        //이진 데이터 저장가능한 객체 선언
+        const url = URL.createObjectURL(blob);
+        //파일 읽어오기 위한 임시URL을 생성(읽어올 객체)
+        //URL 객체 선언
+        const a = document.createElement('a');
+        //a 태그 선언
+        a.href = url;
+        a.download = 'history.csv';
+        //이름,확장자 변경가능, txt
+        a.click();
+        URL.revokeObjectURL(url); // URL 객체 해제
+    } else {
+        alert('다운로드할 이력이 없습니다.!!');
+    }
+}
---
+++ js/keyboardTyping.js
@@ -0,0 +1,60 @@
+var numeric = false ;
+//비교할 전역변수 설정
+function add(char){
+    if(numeric== false){
+
+        if(isNaN(char)==true){
+            //연산자가 두번나올경우에는 입력하지 않습니다.
+        }else{
+            document.getElementById('addSubject').value += char;
+        }
+    }else{
+        document.getElementById('addSubject').value += char;
+    }
+
+    if(isNaN(char) == true){
+        numeric = false ;
+    }else {
+        numeric = true ;
+    }
+}
+
+
+function onlyNumeric(event){
+    
+    // const choPattern = /[\u3131-\u314E]/g;  // 한글 초성의 유니코드 범위
+    // const joongPattern = /[\u314F-\u3163]/g;  // 한글 중성의 유니코드 범위
+    // const jongPattern = /[\u11A8-\u11C2]/g;  // 한글 종성의 유니코드 범위
+    //const koreanPattern = /[\u1100-\u11FF\u3131-\u318E\uAC00-\uD7A3]/g;  // 자모 및 완성형 한글 포함
+    const input = event.target.value;
+        // event.target.value = input.replace(/[가-힣a-zA-Z]/g, '');
+        event.target.value = input.replace(/[A-Za-z\u1100-\u11FF\u3131-\u318E\uAC00-\uD7A3]/g,'');
+        //A-Z 영문대문자
+        //a-z 영문소문자
+        console.log(event);
+        console.log("영문자,한글이 제거되었습니다.")
+        //제어문으로 사칙연산 조사
+        
+
+        // event.target.value = input.replace(choPattern,'');
+        // console.log("한글초성이 제거되었습니다.")
+        // event.target.value = input.replace(joongPattern,'');
+        // console.log("한글중성이 제거되었습니다.")
+        // event.target.value = input.replace(jongPattern,'');
+        // console.log("한글종성이 제거되었습니다.")
+        //정규표현식을 통한 입력시 자동삭제 기능
+        //^\x00-\x7F 기본적인 아스키만 입력가능하게 해주는 유니코드 
+        
+        
+
+
+        
+};
+    
+    
+
+
+
+
+   
+    
